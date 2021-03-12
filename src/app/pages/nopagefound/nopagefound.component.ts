835efa68d37698538720e7692594896ebf8d08de
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2021-03-12 15:50:51 -0500

    estructura de carpetas del proyecto

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.scss'],
+})
+export class NopagefoundComponent {
+  year = new Date().getFullYear();
+  constructor() {}
+}
